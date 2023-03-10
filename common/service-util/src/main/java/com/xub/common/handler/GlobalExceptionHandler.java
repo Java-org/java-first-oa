9e65679d74ce23c8f9aa896d9d7c656f6b401fa8
xub7 <155521373105@163.com>
2023-03-10 09:41:26 +0800

    chapter 1 over

--- common/service-util/src/main/java/com/xub/common/handler/GlobalExceptionHandler.java
+++ common/service-util/src/main/java/com/xub/common/handler/GlobalExceptionHandler.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
+    //@ResponseBody：设置当前控制器方法响应内容为当前返回值，无需解析即如果写在方法上，则将方法的返回内容作为响应体返回给前端
     @ResponseBody
     public Result error(Exception e){
         e.printStackTrace();
